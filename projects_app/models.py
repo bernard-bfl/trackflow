2bc0528659ecca6904047e7aeda9121c1f6d4073
berNard <aggreybernard3@gmail.com>
2026-08-02 23:43:25 +0000

    project model

--- projects_app/models.py
+++ projects_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="projects",)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="member_projects", blank=True,)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        ordering = ["id"]
